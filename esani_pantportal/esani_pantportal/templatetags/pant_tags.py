9b28f76f49465f5f8aec803cb590d46053fc3b6c
Nick Janssen <nj@magenta-aps.dk>
2023-11-20 12:48:22 +0100

    allow superusers to see everything

--- esani_pantportal/esani_pantportal/templatetags/pant_tags.py
+++ esani_pantportal/esani_pantportal/templatetags/pant_tags.py
@@ -37,7 +37,10 @@ def parenthesized(string):
 
 @register.filter(name="has_group")
 def has_group(user, group_name):
-    return user.groups.filter(name=group_name).exists()
+    if user.is_superuser:
+        return True
+    else:
+        return user.groups.filter(name=group_name).exists()
 
 
 @register.filter
