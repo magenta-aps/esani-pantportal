ddf19fc3ce817be0a3469f2b5e85232d8ca10f8c
Nick Janssen <nj@magenta-aps.dk>
2024-01-12 11:09:14 +0100

    feat: [#59113] Show difference between prod and test visually

---
+++ esani_pantportal/esani_pantportal/context_processors.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+from django.conf import settings
+
+
+def environment(request):
+    return {"environment": settings.ENVIRONMENT}
