c25f3c1baf7c0af06cefdc4f4da6a3f5f2f3c200
Mads Riis-Weitling <mrw@magenta.dk>
2024-04-08 14:52:24 +0200

    feat: [#60187] Add "reject" button to product list
    This commits extracts the Javascript code for the "approve" and "delete"
    buttons to an external file, where the code handling the "reject" button
    is added.

---
+++ esani_pantportal/esani_pantportal/static/jquery/product_list_actions.js
@@ -0,0 +1,158 @@
+// SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
+// SPDX-License-Identifier: MPL-2.0
+
+"use strict";
+
+(function () {
+    $(document).ready(function () {
+        const approveButton = $("#approve_button");
+        const rejectButton = $("#reject_button");
+        const deleteButton = $("#delete_button");
+
+        const getRowId = function (row) {
+            const rowId = row.id.toString();
+            // Remove spurious "." that causes type error.
+            return rowId.split(".").join("")
+        }
+
+        const getRows = function () {
+            const selectedRows = $("#table").bootstrapTable("getSelections");
+            const rowIds = [];
+            for (let row of selectedRows) {
+                rowIds.push(getRowId(row));
+            }
+            return rowIds
+        }
+
+        const getUnapprovedRows = function () {
+            const selectedRows = $("#table").bootstrapTable("getSelections");
+            const rowIds = [];
+            for (let row of selectedRows) {
+                if (row.status === "afventer") {
+                    rowIds.push(getRowId(row));
+                }
+            }
+            return rowIds;
+        }
+
+        const updateProducts = function (url, ids) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: {
+                    csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+                    ids: ids
+                },
+                success: function (result) {
+                    $table.bootstrapTable("refresh");
+                    alert(
+                        interpolate(
+                            ngettext(
+                                "Behandlede %(updated)s produkt ud af %(total)s",
+                                "Behandlede %(updated)s produkter ud af %(total)s",
+                                result.updated,
+                            ),
+                            // interpolation context
+                            {
+                                "updated": result.updated,
+                                "total": result.total,
+                            },
+                            true,  // use named interpolation
+                        )
+                    );
+                }
+            });
+        }
+
+        const confirmAndUpdateSelected = function (url, rowIds, message) {
+            if (rowIds.length > 0) {
+                // The `interpolate` function is provided by the Django
+                // `javascript-catalog` view, which returns a JS file.
+                const text = interpolate(
+                    message,  // result of `ngettext` call
+                    {"num": rowIds.length},  // interpolation context
+                    true,  // use named interpolation
+                );
+
+                const confirmed = confirm(text);
+                if (confirmed) {
+                    updateProducts(url, rowIds);
+                }
+            }
+        }
+
+        const onUpdateSelected = function () {
+            const unapprovedRowIds = getUnapprovedRows();
+            const rowIds = getRows();
+
+            if (unapprovedRowIds.length > 0) {
+                approveButton.removeClass("disabled");
+            } else {
+                approveButton.addClass("disabled");
+            }
+
+            if (rowIds.length > 0) {
+                deleteButton.removeClass("disabled");
+                rejectButton.removeClass("disabled");
+            } else {
+                deleteButton.addClass("disabled");
+                rejectButton.addClass("disabled");
+            }
+        }
+
+        approveButton.on("click", function () {
+            const rowIds = getUnapprovedRows();
+
+            confirmAndUpdateSelected(
+                $(this).data("post-url"),
+                rowIds,
+                // The `ngettext` function is provided by the Django
+                // `javascript-catalog` view, which returns a JS file.
+                ngettext(
+                    "Er du sikker på at du vil godkende %(num)s produkt?",
+                    "Er du sikker på at du vil godkende %(num)s produkter?",
+                    rowIds.length
+                )
+            );
+        });
+
+        rejectButton.on("click", function () {
+            const rowIds = getRows();
+
+            confirmAndUpdateSelected(
+                $(this).data("post-url"),
+                rowIds,
+                // The `ngettext` function is provided by the Django
+                // `javascript-catalog` view, which returns a JS file.
+                ngettext(
+                    "Er du sikker på at du vil afvise %(num)s produkt?",
+                    "Er du sikker på at du vil afvise %(num)s produkter?",
+                    rowIds.length,
+                )
+            );
+        });
+
+        deleteButton.on("click", function () {
+            const rowIds = getRows();
+
+            confirmAndUpdateSelected(
+                $(this).data("post-url"),
+                rowIds,
+                // The `ngettext` function is provided by the Django
+                // `javascript-catalog` view, which returns a JS file.
+                ngettext(
+                    "Er du sikker på at du vil slette %(num)s produkt?",
+                    "Er du sikker på at du vil slette %(num)s produkter?",
+                    rowIds.length,
+                )
+            );
+        });
+
+        $("table").on(
+            "uncheck.bs.table uncheck-all.bs.table uncheck-some.bs.table check.bs.table check-some.bs.table check-all.bs.table reset-view.bs.table",
+            function (evt) {
+                onUpdateSelected();
+            },
+        );
+    });
+}());
