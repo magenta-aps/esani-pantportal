f61fcb2b7285f6d9f243149d11cfc86c3f4fd5dc
Mads Riis-Weitling <mrw@magenta.dk>
2024-04-11 09:40:21 +0200

    chore: [#60187] Add `ProductAdmin` in Django admin

--- esani_pantportal/esani_pantportal/admin.py
+++ esani_pantportal/esani_pantportal/admin.py
@@ -1,14 +1,16 @@
 # SPDX-FileCopyrightText: 2023 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
-
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from simple_history.admin import SimpleHistoryAdmin
 
 from esani_pantportal.models import (
     DepositPayout,
     DepositPayoutItem,
     ERPCreditNoteExport,
     ERPProductMapping,
+    Product,
 )
 
 
@@ -62,6 +64,72 @@ class ERPProductMappingAdmin(admin.ModelAdmin):
     ordering = ["item_number"]
 
 
+class CreationDateNullFilter(admin.SimpleListFilter):
+    title = _("oprettelsesdato")
+    parameter_name = "cd"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("all", _("alle")),
+            ("null", _("blank")),
+            ("notnull", _("udfyldt")),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "null":
+            return queryset.filter(creation_date__isnull=True)
+        if self.value() == "notnull":
+            return queryset.filter(creation_date__isnull=False)
+
+
+class ApprovalDateNullFilter(admin.SimpleListFilter):
+    title = _("godkendelsesdato")
+    parameter_name = "ad"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("all", _("alle")),
+            ("null", _("blank")),
+            ("notnull", _("udfyldt")),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "null":
+            return queryset.filter(approval_date__isnull=True)
+        if self.value() == "notnull":
+            return queryset.filter(approval_date__isnull=False)
+
+
+class ProductAdmin(SimpleHistoryAdmin):
+    list_display = [
+        "product_name",
+        "barcode",
+        "state",
+        "creation_date",
+        "approval_date",
+    ]
+    list_filter = [
+        "state",
+        CreationDateNullFilter,
+        ApprovalDateNullFilter,
+    ]
+    search_fields = [
+        "product_name",
+        "barcode",
+    ]
+
+    def creation_date(self, obj):
+        return obj.creation_date
+
+    creation_date.admin_order_field = "creation_date"
+
+    def approval_date(self, obj):
+        return obj.approval_date
+
+    approval_date.admin_order_field = "approval_date"
+
+
 admin.site.register(DepositPayout, DepositPayoutAdmin)
 admin.site.register(ERPCreditNoteExport, ERPCreditNoteExportAdmin)
 admin.site.register(ERPProductMapping, ERPProductMappingAdmin)
+admin.site.register(Product, ProductAdmin)
