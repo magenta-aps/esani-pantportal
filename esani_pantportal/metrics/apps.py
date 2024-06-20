58bdbc77d5ed814eeed09df073d7ea9937ef60a8
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-06-20 10:59:04 +0200

    feat: [#61173] added metrics-app to the project which exposes prometheus- and health_check-metrics

---
+++ esani_pantportal/metrics/apps.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from django.apps import AppConfig
+
+
+class MetricsConfig(AppConfig):
+    name = "metrics"
--- esani_pantportal/project/api.py
+++ esani_pantportal/project/api.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from metrics.api import MetricsAPI
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.controller import NinjaJWTDefaultController
 from project.util import ORJSONRenderer
@@ -11,3 +12,4 @@ from esani_pantportal.api import ApprovedProductsAPI, QRBagAPI, QRStatusAPI
 api = NinjaExtraAPI(title="ESANI Pant", renderer=ORJSONRenderer(), csrf=False)
 api.register_controllers(NinjaJWTDefaultController)
 api.register_controllers(ApprovedProductsAPI, QRBagAPI, QRStatusAPI)
+api.register_controllers(MetricsAPI)
