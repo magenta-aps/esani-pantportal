2ddde6d1de8b6cc8198fb37178bcf1e7e10d4c0b
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-06-21 11:41:02 +0200

    test(metrics): [#61173] new test for MetricsAPI.get_all

---
+++ esani_pantportal/metrics/tests.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2023 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from unittest.mock import MagicMock, patch
+
+from django.test import TestCase
+from prometheus_client import generate_latest
+
+
+class MetricsAPITest(TestCase):
+    def test_get_all(self):
+        resp = self.client.get("/api/metrics")
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content, generate_latest())
