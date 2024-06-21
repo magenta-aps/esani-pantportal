d2ee16961beaaa962854d4e28ea935c5ad37db67
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-06-21 12:01:16 +0200

    test(metrics): [#61173] new tests to verifiy exceptions on health_check endpoints

--- esani_pantportal/metrics/tests.py
+++ esani_pantportal/metrics/tests.py
@@ -19,7 +19,23 @@ class MetricsAPITest(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.content, b"OK")
 
+    @patch("tempfile.NamedTemporaryFile")
+    def test_health_storage_value_error(self, mock_tempfile):
+        mock_tempfile.read = MagicMock(return_value=b"invalid_content")
+        resp = self.client.get("/api/metrics/health/storage")
+
+        self.assertEqual(resp.status_code, 500)
+        self.assertEqual(resp.content, b"ERROR")
+
     def test_health_database(self):
         resp = self.client.get("/api/metrics/health/database")
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.content, b"OK")
+
+    @patch("django.db.connection.ensure_connection")
+    def test_health_database_exception(self, mock_ensure_connection):
+        mock_ensure_connection.side_effect = Exception("Database connection failed")
+        resp = self.client.get("/api/metrics/health/database")
+
+        self.assertEqual(resp.status_code, 500)
+        self.assertEqual(resp.content, b"ERROR")
