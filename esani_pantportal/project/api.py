fa25ea64533c1c4b7892280b98048fdf43f5151b
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-06-25 14:36:19 +0200

    refactor(health-checks): [#61173] 'metrics'-app removed from project.api
    The 'metrics'-app needs to be excluded from the '/api'-prefix, so we can
    exclude it from the public in 'traefik'.. without causing issues with the ACME provider.

--- esani_pantportal/project/api.py
+++ esani_pantportal/project/api.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-from metrics.api import MetricsAPI
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.controller import NinjaJWTDefaultController
 from project.util import ORJSONRenderer
@@ -12,4 +11,3 @@ from esani_pantportal.api import ApprovedProductsAPI, QRBagAPI, QRStatusAPI
 api = NinjaExtraAPI(title="ESANI Pant", renderer=ORJSONRenderer(), csrf=False)
 api.register_controllers(NinjaJWTDefaultController)
 api.register_controllers(ApprovedProductsAPI, QRBagAPI, QRStatusAPI)
-api.register_controllers(MetricsAPI)
