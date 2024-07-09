cd0081cc60e73a43273e4ceb5c936a0a48fe3813
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-07-09 09:22:51 +0200

    test(metrics): [#61173] new test_push_groenland_job_metric_exception

--- esani_pantportal/metrics/tests.py
+++ esani_pantportal/metrics/tests.py
@@ -5,6 +5,7 @@
 from unittest.mock import MagicMock, patch
 
 from django.test import TestCase
+from metrics.job import push_groenland_job_metric
 
 
 class MetricsTest(TestCase):
@@ -33,3 +34,10 @@ class MetricsTest(TestCase):
         resp = self.client.get("/metrics/health/database")
         self.assertEqual(resp.status_code, 500)
         self.assertEqual(resp.content, b"ERROR")
+
+    def test_push_groenland_job_metric_exception(self):
+        with patch("metrics.job.push_to_gateway") as mock_push_to_gateway:
+            mock_push_to_gateway.side_effect = Exception("Push to gateway failed")
+
+            with self.assertRaises(Exception):
+                push_groenland_job_metric("job_name")
