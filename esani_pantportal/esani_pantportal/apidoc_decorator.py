7d37cc991681aefb92e21dfcd3743293223cbceb
Lars Peter Thomsen <larsp@magenta.dk>
2025-01-29 10:47:38 +0100

    feat: [#63711] Testing

--- esani_pantportal/esani_pantportal/apidoc_decorator.py
+++ esani_pantportal/esani_pantportal/apidoc_decorator.py
@@ -1,15 +1,22 @@
+# SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
 from functools import partial
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
+from ninja.main import NinjaAPI
 
 
 def swagger_csp(view_func=None):
     # This gets called whenever django ninja wants to render docs
     # We hook into it by specifying docs_decorator in the NinjaExtraAPI constructor
 
-    if view_func.func.__module__ == "ninja.openapi.views" and view_func.func.__name__ == "openapi_view":
+    if (
+        view_func.func.__module__ == "ninja.openapi.views"
+        and view_func.func.__name__ == "openapi_view"
+    ):
         # If the called function is the one that renders the swagger html
         def openapi_view_csp(request: HttpRequest, api: "NinjaAPI") -> HttpResponse:
             # Return a copy of the template that has CSP attributes in the correct places
@@ -21,7 +28,11 @@ def swagger_csp(view_func=None):
             return render(request, view_tpl, context)
 
         # Insert the replacement with a closure containing the original function arguments
-        return partial(openapi_view_csp, *view_func.args, **view_func.keywords)
+        return partial(
+            openapi_view_csp,
+            *view_func.args,
+            **view_func.keywords,
+        )
 
     else:
         return view_func
---
+++ esani_pantportal/esani_pantportal/tests/test_apidoc.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+import re
+
+from bs4 import BeautifulSoup
+from django.test import TestCase
+
+
+class ApiDocTest(TestCase):
+
+    def test_nonce(self):
+        response = self.client.get("/api/docs")
+        nonce = re.search(r"'(nonce-[\w+=]+)'", response.headers["Content-Security-Policy"]).group(1)
+        self.assertIsNotNone(nonce)
+        dom = BeautifulSoup(response.content, "html.parser")
+        for script_tag in dom.find_all("script"):
+            self.assertTrue(script_tag.has_attr("nonce"))
+            self.assertEqual(script_tag["nonce"], nonce)
+        for script_tag in dom.find_all("link"):
+            self.assertTrue(script_tag.has_attr("nonce"))
+            self.assertEqual(script_tag["nonce"], nonce)
